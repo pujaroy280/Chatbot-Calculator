f7507faf21a571be74a0c84c316340fcf6ed2d6e
Puja Roy <62675121+pujaroy280@users.noreply.github.com>
2021-09-26 17:52:53 -0400

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,24 @@
+#### BEFORE YOU START #### 
+
+## Decide a theme / personality for your chatbot. 
+
+## Decide what sort of questions you are going to ask your user. What information will you be gathering?
+# 1 + 2 = 3#
+
+num1 = int(input("enter your first number please: "))
+num2 = int(input("enter your second number please: "))
+num3 = int(input("enter your third number please"))
+
+print("If you ADD the numbers, you'll get:")
+print(num1 + num2)
+
+print("If you SUBTRACT the numbers, you'll get:")
+print(num1 - num2)
+
+print("If you MULTIPLY the numbers, you'll get:")
+print(num1 * num2)
+
+print("If you DIVIDE the numbers, you'll get:")
+print(num1 / num2)
+
+
